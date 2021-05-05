c1b8e9478f5e9aabd4b0f5fd2c1dc9d5256a91cf
sidney68 <sidney.lee@voiapp.io>
2021-05-06 01:44:14 +0200

    refactor fibonacci

--- a-tour-of-go/fibonacci-closure/fibonacci-closure.go
+++ a-tour-of-go/fibonacci-closure/fibonacci-closure.go
@@ -3,25 +3,23 @@ package main
 import "fmt"
 
 func fibonacci() func() int {
-	var p1, p2, curr int
-	return func() int {
-		switch {
-		case curr == 0:
-			curr = 1
-		case p1 == 0:
-			p1 = 1
-		default:
-			curr = p1 + p2
-			p2 = p1
-			p1 = curr
-		}
-		return p1
-	}
+    var p1, p2 int
+    return func() int {
+        curr := p1 + p2
+        switch {
+        case p1 == 0 && p2 == 0:
+            p1 = 1
+        default:
+            p1 = p2
+            p2 = curr
+        }
+        return curr
+    }
 }
 
 func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
-	}
+    f := fibonacci()
+    for i := 0; i < 10; i++ {
+        fmt.Println(f())
+    }
 }
