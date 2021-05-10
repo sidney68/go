4004d654955daae55eca2e3f9c9e17b2a34cd150
sidney68 <sidney.lee@voiapp.io>
2021-05-10 11:18:45 +0200

    example: equivalent binary trees

---
+++ a-tour-of-go/equivalency-binary-trees/equivalency-binary-trees.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/tour/tree"
+)
+
+func walk(t *tree.Tree, c chan int) {
+	if t.Left != nil {
+		walk(t.Left, c)
+	}
+	c <- t.Value
+	if t.Right != nil {
+		walk(t.Right, c)
+	}
+}
+
+func Walk(t *tree.Tree, c chan int) {
+	walk(t, c)
+	close(c)
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+	c1, c2 := make(chan int), make(chan int)
+	go Walk(t1, c1)
+	go Walk(t2, c2)
+	for i := range c1 {
+		if i != <-c2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	c := make(chan int)
+	go Walk(tree.New(1), c)
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-c)
+	}
+	println("Same(tree.New(1), tree.New(1))", Same(tree.New(1), tree.New(1)))
+	println("Same(tree.New(1), tree.New(2))", Same(tree.New(1), tree.New(2)))
+	println("Same(tree.New(2), tree.New(1))", Same(tree.New(2), tree.New(1)))
+	println("Same(tree.New(2), tree.New(2))", Same(tree.New(2), tree.New(2)))
+}
